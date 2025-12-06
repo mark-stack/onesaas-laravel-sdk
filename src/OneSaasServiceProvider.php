f6e3ab37dfa204346c28f6fb81c857643838bef9
Mark Evans <mark@allegianttechnology.net>
2025-12-06 10:29:14 +0400

    Initial SDK structure

---
+++ src/OneSaasServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Markevans\OnesaasLaravelSdk;
+
+use Illuminate\Support\ServiceProvider;
+
+class OneSaasServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        // This is where your SDK registers routes, commands, etc.
+    }
+}
